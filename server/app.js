e566d4299d3497080ccaf7b0565ff7ae852e67e0
Shreya Dutta <mac_dutta@Shreyas-MacBook-Air.local>
2026-06-28 11:51:16 +0530

    Add root route for backend

--- server/app.js
+++ server/app.js
@@ -1,19 +1,22 @@
 import express from "express";
-
 import cors from "cors";
 
 import questionRoutes from "./routes/questionRoutes.js";
-
 import feedbackRoutes from "./routes/feedbackRoutes.js";
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
-app.use("/api/questions", questionRoutes);
+app.get("/", (req, res) => {
+  res.json({
+    status: "success",
+    message: "AI Interview Prep Backend is Running ",
+  });
+});
 
+app.use("/api/questions", questionRoutes);
 app.use("/api/feedback", feedbackRoutes);
 
 export default app;
